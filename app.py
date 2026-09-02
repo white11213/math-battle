ae24096d3ac6d91ed452e08bddb34953b3038d2d
white11213 <t.yusei1121@gmail.com>
2026-09-03 00:32:16 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -158,6 +158,5 @@ def handle_disconnect():
     if sid in game_state['players']:
         del game_state['players'][sid]
         emit('update_players', get_leaderboard(), broadcast=True)
-
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
